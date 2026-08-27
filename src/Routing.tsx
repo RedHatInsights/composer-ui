50b2564eff61e41f587a1e1de6cf45c18a0cd38c
Luke Green <lgreen@redhat.com>
2026-08-27 13:56:27 -0500

    Create from starter app (#1)
    This creates the project structure based on the starter app. Because
    this repo already existed it just had to be copied over, but it has been
    copied and updated to render a Subscription Composer page at
    /subscriptions/workspaces

---
+++ src/Routing.tsx
@@ -0,0 +1,67 @@
+import { Suspense, lazy, useMemo } from 'react';
+import type { ComponentType } from 'react';
+import { Route as RouterRoute, Routes as RouterRoutes } from 'react-router-dom';
+import { InvalidObject } from '@redhat-cloud-services/frontend-components/InvalidObject';
+import { Bullseye, Spinner } from '@patternfly/react-core';
+
+const ComposerPage = lazy(
+  () => import(/* webpackChunkName: "SamplePage" */ './Pages/ComposerPage/ComposerPage')
+);
+const OopsPage = lazy(() => import(/* webpackChunkName: "OopsPage" */ './Pages/OopsPage/OopsPage'));
+const NoPermissionsPage = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "NoPermissionsPage" */ './Pages/NoPermissionsPage/NoPermissionsPage'
+    )
+);
+
+const routes = [
+  {
+    path: 'no-permissions',
+    element: NoPermissionsPage
+  },
+  {
+    path: 'oops',
+    element: OopsPage
+  },
+  {
+    path: '/',
+    element: ComposerPage
+  },
+  /* Catch all unmatched routes */
+  {
+    path: '*',
+    element: InvalidObject
+  }
+];
+
+interface RouteType {
+  path?: string;
+  element: ComponentType;
+  childRoutes?: RouteType[];
+  elementProps?: Record<string, unknown>;
+}
+
+const renderRoutes = (routes: RouteType[] = []) =>
+  routes.map(({ path, element: Element, childRoutes, elementProps }) => (
+    <RouterRoute key={path} path={path} element={<Element {...elementProps} />}>
+      {renderRoutes(childRoutes)}
+    </RouterRoute>
+  ));
+
+const Routing = () => {
+  const renderedRoutes = useMemo(() => renderRoutes(routes), [routes]);
+  return (
+    <Suspense
+      fallback={
+        <Bullseye>
+          <Spinner />
+        </Bullseye>
+      }
+    >
+      <RouterRoutes>{renderedRoutes}</RouterRoutes>
+    </Suspense>
+  );
+};
+
+export default Routing;
